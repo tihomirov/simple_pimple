ed5581754fb8c838f3f54536a9f804c7fb474c88
tihomirov <assatihomirov9@gmail.com>
2016-05-12 18:07:57 +0300

    improve creating post

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,7 +1,20 @@
 Posts = new Mongo.Collection('posts');
 
-Posts.allow({
-	insert: function (userId, doc) {
-		return !!userId;
+Meteor.methods({
+	postInsert: function(postAttributes) {
+		check(Meteor.userId(), String);
+		check(postAttributes, {
+			title: String,
+			url: String
+		});
+		var user = Meteor.user();
+		var post = _.extend(postAttributes, {
+			authorId: user._id,
+			submitted: new Date()
+		});
+		var postId = Posts.insert(post);
+		return {
+			_id: postId
+		};
 	}
 });
